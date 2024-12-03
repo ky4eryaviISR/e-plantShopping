2d4b4de17fb74d3df307ed54a64133b117584543
Vladimir <you@example.com>
2024-12-03 02:12:46 -0500

    init

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './AboutUs.css';
 
 function AboutUs() {
-  return (
+  return ( 
     <div className="about-us-container">
       {/* <h1 className="about-us-heading">About Us</h1> */}
       <p className="about-us-description">Welcome to Paradise Nursery, where green meets serenity!</p>
